7d3fc506580b92fb9924892b9145245a457261b5
Prateek Sharma <nonuprateek1996@gmail.com>
2026-08-29 11:25:31 +0530

    Add visible Indeed and Firecrawl source tabs

---
+++ web/src/app/sources/page.tsx
@@ -0,0 +1,7 @@
+import { SourceSearchView } from "@/components/sources/source-search-view";
+
+export const dynamic = "force-dynamic";
+
+export default function SourcesPage() {
+  return <SourceSearchView />;
+}
